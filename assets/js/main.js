fd87efb0f2ba8754e2307e599cc37970a3638688
Justin Grant <jlgrock@gmail.com>
2019-11-22 18:34:39 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,6 @@ var redirects = {
 (function() {
   var s = document.location.search;
   if (s in redirects) {
-    window.location.replace('/' + redirects[s]);
+    window.location.replace(redirects[s]);
   }
 })();
